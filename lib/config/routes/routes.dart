7ffc2a7d3761e878cf509ce8c001032ee20a3c35
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2022-12-23 14:28:14 +0700

    feat: add stacked route

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -2,12 +2,19 @@
 // @AdaptiveAutoRouter
 // @CustomAutoRouter
 import 'package:auto_route/auto_route.dart';
+import 'package:mynewsapp/module/home/screens/home_layout.dart';
 import 'package:mynewsapp/module/home/screens/home_screen.dart';
+import 'package:mynewsapp/module/saved/screens/saved_screens.dart';
+import 'package:mynewsapp/module/search/screens/search_screens.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Screen,Route',
   routes: <AutoRoute>[
-    AutoRoute(page: HomeScreen, initial: true),
+    AutoRoute(page: HomeLayout, initial: true, children: [
+      AutoRoute(page: HomeScreen, path: "/map"),
+      AutoRoute(page: SearchScreen, path: "/search"),
+      AutoRoute(page: SavedScreen, path: "/saved"),
+    ]),
   ],
 )
 class $AppRouter {}
