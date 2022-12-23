2cf3658ec85a73e4ced438671dcc352a0f18119c
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2022-12-23 11:09:50 +0700

    feat: add saved and search screen

---
+++ lib/module/saved/screens/saved_screens.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SavedScreen extends StatelessWidget {
+  const SavedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Saved")));
+  }
+}
---
+++ lib/module/search/screens/search_screens.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Search")));
+  }
+}
