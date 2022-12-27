c683412e60e5d0e5675fd15a3f887c8ca3320766
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2022-12-27 11:58:03 +0700

    feat: create api path for news api

---
+++ lib/constant/api_path.dart
@@ -0,0 +1,6 @@
+class ApiPath {
+  ApiPath._();
+  static const String getHeadlinesNews = "/top-headlines";
+  static const String getNews = "/everything";
+  static const String getSources = "/top-headlines/sources";
+}
