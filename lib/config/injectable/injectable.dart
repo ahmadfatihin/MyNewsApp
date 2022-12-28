eedb383e0a75a8513d9f012603f521894d0d9523
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2022-12-29 02:19:07 +0700

    feat: create navigation services

--- lib/config/injectable/injectable.dart
+++ lib/config/injectable/injectable.dart
@@ -1,9 +1,13 @@
 import 'package:get_it/get_it.dart';
 import 'package:mynewsapp/constant/api_path.dart';
+import 'package:mynewsapp/services/navigation_services/navigation_services.dart';
 
 GetIt inject = GetIt.instance;
 
 Future<void> setupLocator() async {
   /// Registering api endpoint
   inject.registerSingleton(ApiPath);
+
+  /// Registering navigation service
+  inject.registerSingleton(NavigationServices());
 }
